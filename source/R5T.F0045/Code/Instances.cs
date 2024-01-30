195b616d556f1b3f873a329505f8534359f82df4
davidcoats <david@getsafetycone.com>
2024-01-29 18:32:59 -0800

    Interim changes.

--- source/R5T.F0045/Code/Instances.cs
+++ source/R5T.F0045/Code/Instances.cs
@@ -1,35 +1,22 @@
 using System;
 
-using R5T.F0020;
-using R5T.F0024;
-using R5T.F0040.F000;
-using R5T.F0042;
-using R5T.F0043;
-using R5T.F0046;
-using R5T.F0048;
-using R5T.F0049;
-using R5T.F0051;
-using R5T.F0055;
-using R5T.F0056;
-using R5T.F0063;
-
 
 namespace R5T.F0045
 {
     public static class Instances
     {
-        public static ILibraryDescriptionOperator LibraryDescriptionOperator { get; } = F0043.LibraryDescriptionOperator.Instance;
-        public static ILibraryNameOperator LibraryNameOperator { get; } = F0043.LibraryNameOperator.Instance;
-        public static IProjectFileOperator ProjectFileOperator { get; } = F0020.ProjectFileOperator.Instance;
-        public static IProjectNamespacesOperator ProjectNamespacesOperator { get; } = F0040.F000.ProjectNamespacesOperator.Instance;
+        public static F0043.ILibraryDescriptionOperator LibraryDescriptionOperator { get; } = F0043.LibraryDescriptionOperator.Instance;
+        public static F0043.ILibraryNameOperator LibraryNameOperator { get; } = F0043.LibraryNameOperator.Instance;
+        public static F0020.IProjectFileOperator ProjectFileOperator { get; } = F0020.ProjectFileOperator.Instance;
+        public static F0040.F000.IProjectNamespacesOperator ProjectNamespacesOperator { get; } = F0040.F000.ProjectNamespacesOperator.Instance;
         public static F0051.IProjectOperator ProjectOperator { get; } = F0051.ProjectOperator.Instance;
-        public static IProjectNameOperator ProjectNameOperator { get; } = F0055.ProjectNameOperator.Instance;
-        public static IProjectOperations ProjectOperations { get; } = F0056.ProjectOperations.Instance;
-        public static IRepositoryDescriptionOperator RepositoryDescriptionOperator { get; } = F0046.RepositoryDescriptionOperator.Instance;
-        public static IRepositoryNameOperator RepositoryNameOperator { get; } = F0046.RepositoryNameOperator.Instance;
+        public static F0055.IProjectNameOperator ProjectNameOperator { get; } = F0055.ProjectNameOperator.Instance;
+        public static F0056.IProjectOperations ProjectOperations { get; } = F0056.ProjectOperations.Instance;
+        public static F0046.IRepositoryDescriptionOperator RepositoryDescriptionOperator { get; } = F0046.RepositoryDescriptionOperator.Instance;
+        public static F0046.IRepositoryNameOperator RepositoryNameOperator { get; } = F0046.RepositoryNameOperator.Instance;
         public static F0056.IRepositoryOperator RepositoryOperator { get; } = F0056.RepositoryOperator.Instance;
         public static F0063.ISolutionFileOperator SolutionFileOperator { get; } = F0063.SolutionFileOperator.Instance;
-        public static ISolutionNameOperator SolutionNameOperator { get; } = F0048.SolutionNameOperator.Instance;
+        public static F0048.ISolutionNameOperator SolutionNameOperator { get; } = F0048.SolutionNameOperator.Instance;
         public static F0049.ISolutionOperator SolutionOperator { get; } = F0049.SolutionOperator.Instance;
         public static F0063.ISolutionOperations SolutionOperations { get; } = F0063.SolutionOperations.Instance;
     }
